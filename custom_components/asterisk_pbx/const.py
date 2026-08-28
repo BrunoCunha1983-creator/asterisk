cb1a00534c0e01014640112519979219ab89edfc
BrunoCunha1983-creator <brunocunhatlm@gmail.com>
2026-08-28 03:12:08 +0100

    Add Asterisk PBX integration constants

---
+++ custom_components/asterisk_pbx/const.py
@@ -0,0 +1,6 @@
+DOMAIN = "asterisk_pbx"
+DEFAULT_HOST = "homeassistant.local"
+DEFAULT_PORT = 8099
+DEFAULT_SCAN_INTERVAL = 15
+CONF_SCAN_INTERVAL = "scan_interval"
+PLATFORMS = ["sensor", "binary_sensor"]
