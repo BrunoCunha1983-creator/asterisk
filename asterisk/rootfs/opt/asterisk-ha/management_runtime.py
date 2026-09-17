e2cb07d895237cac2661b10b490d7c22352af6a3
BrunoCunha1983-creator <brunocunhatlm@gmail.com>
2026-09-17 07:54:12 +0100

    Wire ARI diagnostics into runtime UI

---
+++ asterisk/rootfs/opt/asterisk-ha/management_runtime.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+from urllib.parse import urlparse
+
+from ari_runtime import ARI
+from ari_ui import augment_index as augment_ari_index
+
+
+def install(app):
+    """Install ARI diagnostics over the already assembled server_entry module."""
+    app.server.INDEX = augment_ari_index(app.server.INDEX)
+
+    base_handler = app.H
+
+    class ManagementHandler(base_handler):
+        def do_GET(self):
+            path = urlparse(self.path).path.rstrip('/') or '/'
+            if path == '/api/ari-status':
+                if not self._guard_web():
+                    return
+                try:
+                    out = ARI.status()
+                    http_runtime = app.server.ast('http show status')
+                    ari_runtime = app.server.ast('ari show status')
+                    ari_users = app.server.ast('ari show users')
+                    out['http_runtime'] = http_runtime.get('output', '')
+                    out['ari_runtime'] = ari_runtime.get('output', '')
+                    out['ari_users'] = ari_users.get('output', '')
+                    self.sendj(out)
+                except Exception as exc:
+                    self.sendj({'connected': False, 'error': str(exc), 'password_exposed': False}, 500)
+                return
+            super().do_GET()
+
+    app.H = ManagementHandler
